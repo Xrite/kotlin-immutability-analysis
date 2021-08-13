20b8f5812d07766289d0585b6ecdbe47bb2b411d
Anton Bukov <bukov.anton@gmail.com>
2021-08-13 18:18:09 +0500

    use another csv library

--- kotlin-analysis-immutability/src/main/kotlin/test/test/output/CSVWriterResourceManager.kt
+++ kotlin-analysis-immutability/src/main/kotlin/test/test/output/CSVWriterResourceManager.kt
@@ -2,49 +2,47 @@ package test.test
 
 import com.github.doyaaaaaken.kotlincsv.client.ICsvFileWriter
 import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVPrinter
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
 import org.jetbrains.research.ml.kotlinAnalysis.ResourceManager
 import java.io.File
+import java.io.FileWriter
 import java.io.OutputStream
 import java.nio.file.Path
 
 class CSVWriterResourceManager(private val directory: Path, private val fileName: String) : ResourceManager {
-    private val header = listOf("project", "name", "type", "immutability", "tests")
-    private val writer = csvWriter { lineTerminator = System.lineSeparator() }
-    private val actions = mutableListOf<ICsvFileWriter.() -> Unit>()
+    private val header = listOf("project", "name", "type", "immutability", "tests", "reason")
     fun addResult(projectName: String, tests: TestsType, immutability: Immutability) {
-        actions.add {
-            immutability.results().forEach {
-                val entity = it.first
-                val result = when (it.second) {
-                    is ImmutabilityStatus.ConditionallyDeeplyImmutable -> "ConditionallyDeeplyImmutable"
-                    is ImmutabilityStatus.Immutable -> "Immutable"
-                    is ImmutabilityStatus.Mutable -> "Mutable"
-                    is ImmutabilityStatus.ShallowImmutable -> "ShallowImmutable"
-                }
-                val name = when (entity) {
-                    is ClassTemplate -> entity.desc.fqNameSafe.asString()
-                    ErrorTemplate -> "ERROR"
-                }
-                val type = when (entity) {
-                    is ClassTemplate -> entity.classType.name
-                    ErrorTemplate -> "ERROR"
-                }
-                writeRow(projectName, name, type, result, tests)
+        immutability.results().forEach {
+            val entity = it.first
+            val result = when (it.second) {
+                is ImmutabilityStatus.ConditionallyDeeplyImmutable -> "ConditionallyDeeplyImmutable"
+                is ImmutabilityStatus.Immutable -> "Immutable"
+                is ImmutabilityStatus.Mutable -> "Mutable"
+                is ImmutabilityStatus.ShallowImmutable -> "ShallowImmutable"
             }
+            val name = when (entity) {
+                is ClassTemplate -> entity.desc.fqNameSafe.asString()
+                ErrorTemplate -> "ERROR"
+            }
+            val type = when (entity) {
+                is ClassTemplate -> entity.classType.name
+                ErrorTemplate -> "ERROR"
+            }
+            printer.printRecord(projectName, name, type, result, tests)
         }
     }
 
-    lateinit var stream: OutputStream
+    private lateinit var printer: CSVPrinter
 
     override fun init() {
-        stream = File(directory.toFile(), fileName).outputStream()
+        printer = CSVPrinter(File(directory.toFile(), fileName).bufferedWriter(), CSVFormat.DEFAULT)
+        printer.printRecord(header)
     }
 
     override fun close() {
-        writer.open(stream) {
-            writeRow(header)
-            actions.forEach { it() }
-        }
+        printer.flush()
+        printer.close()
     }
 }
