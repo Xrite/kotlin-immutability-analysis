84bc83b6bf503c09113e7cbfc044dfac20683f47
Anton Bukov <bukov.anton@gmail.com>
2021-08-18 01:41:20 +0500

    add different configurations

--- kotlin-analysis-plugin/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/KotlinImmutabilityAnalysisRunner.kt
+++ kotlin-analysis-plugin/src/main/kotlin/org/jetbrains/research/ml/kotlinAnalysis/KotlinImmutabilityAnalysisRunner.kt
@@ -4,25 +4,74 @@ import com.intellij.ide.impl.ProjectUtil
 import com.intellij.openapi.externalSystem.importing.ImportSpecBuilder
 import com.intellij.openapi.externalSystem.util.ExternalSystemUtil
 import com.intellij.openapi.project.Project
+import com.xenomachina.argparser.ArgParser
 import org.jetbrains.idea.maven.project.MavenProjectsManager
 import org.jetbrains.plugins.gradle.util.GradleConstants
+import test.test.Configuration
 import test.test.ImmutabilityAnalysisExecutor
 import java.nio.file.Paths
 
-object ImmutabilityArgs : RunnerArgs {
-
-}
-
-object ImmutabilityArgsParser : RunnerArgsParser<ImmutabilityArgs> {
-    override fun parse(args: List<String>): ImmutabilityArgs {
-        TODO("Not yet implemented")
-    }
-}
 
 object KotlinImmutabilityAnalysisRunner : KotlinAnalysisRunner<IORunnerArgs, IORunnerArgsParser>
     ("kotlin-immutability-analysis", IORunnerArgsParser) {
     override fun run(args: IORunnerArgs) {
-        ImmutabilityAnalysisExecutor(args.outputDir).execute(args.inputDir) { projectPath ->
+        val configurations = listOf(
+            Configuration(
+                treatCollectionsAsMutable = false,
+                treatLazyAsImmutable = true,
+                analyzeSealedSubclasses = false,
+                assumeNastyInheritors = false,
+                assumeGoodGetters = false,
+                includeTests = false,
+                outputFileName = "results_default_without_tests.csv"
+            ),
+            Configuration(
+                treatCollectionsAsMutable = false,
+                treatLazyAsImmutable = true,
+                analyzeSealedSubclasses = false,
+                assumeNastyInheritors = false,
+                assumeGoodGetters = false,
+                includeTests = true,
+                outputFileName = "results_default_with_tests.csv"
+            ),
+            Configuration(
+                treatCollectionsAsMutable = true,
+                treatLazyAsImmutable = true,
+                analyzeSealedSubclasses = true,
+                assumeNastyInheritors = true,
+                assumeGoodGetters = false,
+                includeTests = false,
+                outputFileName = "results_pessimistic_without_tests.csv"
+            ),
+            Configuration(
+                treatCollectionsAsMutable = true,
+                treatLazyAsImmutable = true,
+                analyzeSealedSubclasses = true,
+                assumeNastyInheritors = true,
+                assumeGoodGetters = false,
+                includeTests = true,
+                outputFileName = "results_pessimistic_with_tests.csv"
+            ),
+            Configuration(
+                treatCollectionsAsMutable = false,
+                treatLazyAsImmutable = true,
+                analyzeSealedSubclasses = false,
+                assumeNastyInheritors = false,
+                assumeGoodGetters = true,
+                includeTests = false,
+                outputFileName = "results_optimistic_without_tests.csv"
+            ),
+            Configuration(
+                treatCollectionsAsMutable = false,
+                treatLazyAsImmutable = true,
+                analyzeSealedSubclasses = false,
+                assumeNastyInheritors = false,
+                assumeGoodGetters = true,
+                includeTests = true,
+                outputFileName = "results_optimistic_with_tests.csv"
+            ),
+        )
+        ImmutabilityAnalysisExecutor(args.outputDir, configurations).execute(args.inputDir) { projectPath ->
             setUpProject(projectPath.toString())
         }
     }
