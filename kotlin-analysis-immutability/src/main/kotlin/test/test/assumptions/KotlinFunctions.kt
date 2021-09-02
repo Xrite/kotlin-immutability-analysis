708acd2d5c61a759ac8bf35fcf34215200492380
Anton Bukov <bukov.anton@gmail.com>
2021-09-02 13:39:59 +0500

    add Function and FunctionN

--- kotlin-analysis-immutability/src/main/kotlin/test/test/assumptions/KotlinFunctions.kt
+++ kotlin-analysis-immutability/src/main/kotlin/test/test/assumptions/KotlinFunctions.kt
@@ -5,9 +5,10 @@ import test.test.ImmutabilityProperty
 
 object KotlinFunctions : Assumptions {
     override fun get(name: String): ImmutabilityProperty? =
-        if ("kotlin.Function(0|[1-9][0-9]*)".toRegex().matches(name)) {
-            ImmutabilityProperty.Immutable()
-        } else {
-            null
+        when {
+            "kotlin.Function(0|[1-9][0-9]*)".toRegex().matches(name) -> ImmutabilityProperty.Immutable()
+            name == "kotlin.jvm.functions.FunctionN" -> ImmutabilityProperty.Immutable()
+            name == "kotlin.Function" -> ImmutabilityProperty.Immutable()
+            else -> null
         }
 }
